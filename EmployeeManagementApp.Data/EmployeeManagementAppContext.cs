67dc6dee9935423a7d9a7ab679d8162405af21a2
vmarcell96 <vajdam96@gmail.com>
2022-08-22 22:25:55 +0200

    Created DbContext class for App

---
+++ EmployeeManagementApp.Data/EmployeeManagementAppContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeManagementApp.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagementApp.Data
+{
+    public class EmployeeManagementAppContext : DbContext
+    {
+        public EmployeeManagementAppContext(DbContextOptions<EmployeeManagementAppContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
