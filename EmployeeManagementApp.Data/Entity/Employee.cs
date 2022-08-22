6b4f215d2b38bfeb28226baf4dcfeb02ec7a1340
vmarcell96 <vajdam96@gmail.com>
2022-08-22 22:23:36 +0200

    Created employee entity

---
+++ EmployeeManagementApp.Data/Entity/Employee.cs
@@ -0,0 +1,12 @@
+﻿
+namespace EmployeeManagementApp.Data.Entity
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime HiringDate { get; set; }
+
+    }
+}
