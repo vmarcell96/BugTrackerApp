e866aff24438bb2b32777ffc6a70bee37012d0bf
vmarcell96 <vajdam96@gmail.com>
2022-08-22 22:24:00 +0200

    Created update,view DTOs to employee entity

---
+++ EmployeeManagementApp.Core/Model/Employees/EmployeeViewDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementApp.Core.Model.Employees
+{
+    public class EmployeeViewDto
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
