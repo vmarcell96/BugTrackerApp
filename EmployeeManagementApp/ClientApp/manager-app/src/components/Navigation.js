d08316bb0b0d3b30f81a12b71c26c16c8c05d61a
vmarcell96 <vajdam96@gmail.com>
2022-08-28 23:53:45 +0200

    added new links to navbar

--- EmployeeManagementApp/ClientApp/manager-app/src/components/Navigation.js
+++ EmployeeManagementApp/ClientApp/manager-app/src/components/Navigation.js
@@ -1,21 +1,30 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { Navbar,Nav } from 'react-bootstrap'
-import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse'
+import useAuth from '../hooks/useAuth';
 
 const Navigation = () => {
+    const { auth, logout } = useAuth();
   return (
     <Navbar bg="dark" expand="lg">
         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav">
-        <Nav>
-            <NavLink className="d-inline p-2 bg-dark text-white" to="/">
-                Home
-            </NavLink>
-            <NavLink className="d-inline p-2 bg-dark text-white" to="/employees">
-                Employees
-            </NavLink>
-        </Nav>
+            <Nav>
+                <NavLink className="d-inline p-2 bg-dark text-white" to="/">
+                    Home
+                </NavLink>
+                {!auth &&<NavLink className="d-inline p-2 bg-dark text-white" to="/login">
+                    Login
+                </NavLink>}
+                {auth &&<NavLink className="d-inline p-2 bg-dark text-white" to="/employees">
+                    Employees
+                </NavLink>}
+                {auth && auth.role === "Admin" &&
+                <NavLink className="d-inline p-2 bg-dark text-white" to="/users">
+                    Users
+                </NavLink>}
+                {auth && <Nav.Link className="d-inline p-2 bg-dark text-white" onClick={logout}>Logout</Nav.Link>}
+            </Nav>
         </Navbar.Collapse>
     </Navbar>
   )
