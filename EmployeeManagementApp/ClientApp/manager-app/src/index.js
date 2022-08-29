f6e62c52444b9fddff81ff3bcdbd62a3a2eb41b2
vmarcell96 <vajdam96@gmail.com>
2022-08-29 18:24:15 +0200

    added flashmessage wrap, new user and employee related routes to react router

--- EmployeeManagementApp/ClientApp/manager-app/src/index.js
+++ EmployeeManagementApp/ClientApp/manager-app/src/index.js
@@ -4,17 +4,22 @@ import './index.css';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AuthProvider from './context/AuthProvider';
-import Employees from './components/Employees';
-import AddEmployee from './components/AddEmployee';
-import Users from './components/Users';
+import Employees from './components/Employees/Employees';
+import AddEmployee from './components/Employees/AddEmployee';
+import Users from './components/Users/Users';
 import Login from './components/Login';
 import Home from './components/Home';
 import RequireAuth from './components/RequireAuth';
+import FlashMessageProvider from './context/FlashMessageProvider';
+import UpdateEmployee from './components/Employees/UpdateEmployee';
+import AddUser from './components/Users/AddUser';
+import UpdateUser from './components/Users/UpdateUser';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <FlashMessageProvider>
       <AuthProvider>
         <Routes>
           <Route path="/" element={<App />}>
@@ -22,10 +27,14 @@ root.render(
             <Route path='/login' element={<Login />} />
             <Route path='/employees' element={<RequireAuth allowedRoles={["Admin","User"]}><Employees /></RequireAuth>}/>
             <Route path='/employees/add' element={<RequireAuth allowedRoles={["Admin"]}><AddEmployee /></RequireAuth>}/>
+            <Route path='/employees/:id' element={<RequireAuth allowedRoles={["Admin"]}><UpdateEmployee /></RequireAuth>}/>
             <Route path='/users' element={<RequireAuth allowedRoles={["Admin"]}><Users /></RequireAuth>}/>
+            <Route path='/users/add' element={<RequireAuth allowedRoles={["Admin"]}><AddUser /></RequireAuth>}/>
+            <Route path='/users/:id' element={<RequireAuth allowedRoles={["Admin"]}><UpdateUser /></RequireAuth>}/>
           </Route>
         </Routes>
       </AuthProvider>
+      </FlashMessageProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
