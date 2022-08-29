6378fa88e29b62ca90c351f3a7930a89a263f62f
vmarcell96 <vajdam96@gmail.com>
2022-08-29 22:46:18 +0200

    deleted dataannotations

--- EmployeeManagementApp.Core/Model/Users/UserCreateDto.cs
+++ EmployeeManagementApp.Core/Model/Users/UserCreateDto.cs
@@ -10,16 +10,12 @@ namespace EmployeeManagementApp.Core.Model.Users
     public class UserCreateDto
     {
         [Required]
-        [StringLength(32, MinimumLength = 4, ErrorMessage = "Username must be between 4 and 32 characters long")]
         public string UserName { get; set; }
         [Required]
-        [StringLength(32, MinimumLength = 2, ErrorMessage = "First name must be between 2 and 32 characters long")]
         public string FirstName { get; set; }
         [Required]
-        [StringLength(32, MinimumLength = 2, ErrorMessage = "Last name must be between 2 and 32 characters long")]
         public string LastName { get; set; }
         [Required]
-        [StringLength(32, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 32 characters long")]
         public string Password { get; set; }
         [Required]
         public string Role { get; set; }
