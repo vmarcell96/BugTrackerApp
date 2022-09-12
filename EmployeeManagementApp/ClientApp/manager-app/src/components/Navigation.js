ef140aa6c0e4ad217276adf9add186bd09a206ac
vmarcell96 <vajdam96@gmail.com>
2022-09-12 21:35:00 +0200

    logo added to navbar,style changed

--- EmployeeManagementApp/ClientApp/manager-app/src/components/Navigation.js
+++ EmployeeManagementApp/ClientApp/manager-app/src/components/Navigation.js
@@ -1,34 +1,45 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 import { LinkContainer } from "react-router-bootstrap";
 import useAuth from '../hooks/useAuth';
 import '../index.css';
+import Logo from '../bug2.png';
 
 const Navigation = () => {
     const { auth, logout } = useAuth();
   return (
-    <Navbar bg="dark" expand="lg">
-        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-        <Navbar.Collapse id="basic-navbar-nav">
-            <Nav>
-                <LinkContainer to="/">
-                    <Navbar.Brand className="d-inline p-2 bg-dark text-white">EmployeeManager</Navbar.Brand>
-                </LinkContainer>
-                {!auth && <LinkContainer to="/login">
-                    <Nav.Link className="d-inline p-2 bg-dark text-white">
-                        Login
-                    </Nav.Link></LinkContainer>}
-                {auth &&<LinkContainer to="/employees"><Nav.Link className="d-inline p-2 bg-dark text-white" >
-                    Employees
-                </Nav.Link></LinkContainer>}
-                {auth && auth.role === "Admin" &&
-                <LinkContainer to="/users"><Nav.Link className="d-inline p-2 bg-dark text-white"> 
-                    Users
-                </Nav.Link></LinkContainer>}
-                {auth && <Nav.Link className="d-inline p-2 bg-dark text-white" onClick={logout}>Logout</Nav.Link>}
-            </Nav>
-        </Navbar.Collapse>
+    <Navbar bg="light" expand="lg">
+        <Container>
+            <LinkContainer to="/">
+                <Navbar.Brand className="d-inline p-2 bg-light text-black align-self-center">
+                    <img 
+                        src={Logo}
+                        width="30"
+                        height="30"
+                        className="d-inline-block bg-light align-top"
+                    />
+                     EmployeeManager
+                </Navbar.Brand>
+            </LinkContainer>
+            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                        {!auth && <LinkContainer to="/login">
+                            <Nav.Link className="d-inline p-2 bg-light text-black align-self-center">
+                                Login
+                            </Nav.Link></LinkContainer>}
+                        {auth &&<LinkContainer to="/employees"><Nav.Link className="d-inline p-2 bg-light text-black align-self-center" >
+                            Employees
+                        </Nav.Link></LinkContainer>}
+                        {auth && auth.role === "Admin" &&
+                        <LinkContainer to="/users"><Nav.Link className="d-inline p-2 bg-light text-black align-self-center"> 
+                            Users
+                        </Nav.Link></LinkContainer>}
+                        {auth && <Nav.Link className="d-inline p-2 bg-light text-black" onClick={logout}>Logout</Nav.Link>}
+                    </Nav>
+            </Navbar.Collapse>
+        </Container>
     </Navbar>
   )
 }
