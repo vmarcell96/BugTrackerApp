f2c11210525db6daee43e5bddc03deb73a6eab4c
vmarcell96 <vajdam96@gmail.com>
2022-09-13 00:44:02 +0200

    implemented refreshtoken repository class with add,get methods

---
+++ EmployeeManagementApp.Data/Repositories/IRefreshTokenRepository.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeManagementApp.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementApp.Data.Repositories
+{
+    public interface IRefreshTokenRepository
+    {
+        Task Add(RefreshToken refreshToken);
+        Task<RefreshToken> GetByToken(string refreshToken);
+    }
+}
