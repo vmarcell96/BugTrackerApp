3f21a02ab349b7ba52d848c7e2feda3fec1f17c5
vmarcell96 <vajdam96@gmail.com>
2022-09-12 21:34:23 +0200

    home component has card now

--- EmployeeManagementApp/ClientApp/manager-app/src/components/Home.js
+++ EmployeeManagementApp/ClientApp/manager-app/src/components/Home.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { Button, Form, Card, Row, Col} from "react-bootstrap";
+import '../home.css'
 
 const Home = () => {
   return (
-  <div className="mt-5 d-flex justify-content-left">
-    Welcome to my page!
-  </div>
+    <Row className='d-flex justify-content-center align-items-center h-100'>
+    <Col col='12'>
+        <Card className='bg-white my-5 mx-auto' style={{borderRadius: '1rem', maxWidth: '500px'}}>
+            <Card.Body className='p-5 w-100 d-flex flex-column'>
+                <Col>
+                  <div className="d-flex justify-content-center">
+                      Welcome to my page!
+                  </div>
+                </Col>
+            </Card.Body>
+        </Card>
+    </Col>
+  </Row>
   )
 };
 
