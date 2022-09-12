7c23d51d41830ebedc03f22822cd86f445789d0f
vmarcell96 <vajdam96@gmail.com>
2022-09-12 21:34:37 +0200

    login component refactored

--- EmployeeManagementApp/ClientApp/manager-app/src/components/Login.js
+++ EmployeeManagementApp/ClientApp/manager-app/src/components/Login.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { Button, Form, Card } from "react-bootstrap";
+import { Button, Form, Card, Row, Col } from "react-bootstrap";
 import useAuth from "../hooks/useAuth";
+import '../login.css'; 
 
 function Login() {
     const [username, setUsername] = useState("");
@@ -13,21 +14,27 @@ function Login() {
     }
 
     return (
-        <Card>
-            <Card.Body>
-                <Form onSubmit={handleSubmit}>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Username</Form.Label>
-                        <Form.Control type="text" onInput={e => setUsername(e.target.value)} required />
-                    </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" onInput={e => setPassword(e.target.value)} required />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">Login</Button>
-                </Form>
-            </Card.Body>
-        </Card>
+        <Row className='d-flex justify-content-center align-items-center h-100'>
+            <Col col='12'>
+                <Card className='bg-white my-5 mx-auto' style={{borderRadius: '1rem', maxWidth: '500px'}}>
+                    <Card.Body className='p-5 w-100 d-flex flex-column'>
+                        <Form onSubmit={handleSubmit}>
+                            <Form.Group className="mb-4 w-100">
+                                <Form.Label>Username</Form.Label>
+                                <Form.Control type="text" onInput={e => setUsername(e.target.value)} required />
+                            </Form.Group>
+                            <Form.Group className="mb-4 w-100">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" onInput={e => setPassword(e.target.value)} required />
+                            </Form.Group>
+                            <div className="d-flex justify-content-center">
+                                <Button variant="primary" type="submit">Login</Button>
+                            </div>
+                        </Form>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </Row>
     );
 }
 
