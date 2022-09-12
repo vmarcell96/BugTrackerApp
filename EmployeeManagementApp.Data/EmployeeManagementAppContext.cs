60921db8037e226a286fa451a2123a5cfe1c1292
vmarcell96 <vajdam96@gmail.com>
2022-09-13 00:47:20 +0200

    added RefreshTokens table to context

--- EmployeeManagementApp.Data/EmployeeManagementAppContext.cs
+++ EmployeeManagementApp.Data/EmployeeManagementAppContext.cs
@@ -12,5 +12,6 @@ namespace EmployeeManagementApp.Data
 
         public DbSet<Employee> Employees { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
     }
 }
