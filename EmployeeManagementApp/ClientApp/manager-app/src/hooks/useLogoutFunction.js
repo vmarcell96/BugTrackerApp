ad48eb040cb2f97dd503ff8e6dca2f097c650922
vmarcell96 <vajdam96@gmail.com>
2022-09-14 23:29:27 +0200

    Logout function is now in a separate hook

---
+++ EmployeeManagementApp/ClientApp/manager-app/src/hooks/useLogoutFunction.js
@@ -0,0 +1,25 @@
+import useAuth from "./useAuth";
+import axios from "../apis/axiosInstance";
+import useAxiosFunction from "./useAxiosFunction";
+import { useNavigate } from "react-router";
+
+const useLogoutFunction = () => {
+    const navigate = useNavigate();
+    const [employees, error, loading, axiosFetch] = useAxiosFunction();
+    const { setAuth } = useAuth();
+    
+    const logout = () => {
+        axiosFetch({
+            axiosInstance: axios,
+            method: 'DELETE',
+            url: '/api/auth/logout',
+        });
+        setAuth(null);
+        localStorage.removeItem("auth");
+        navigate("/");
+    }
+    
+  return logout;
+}
+
+export default useLogoutFunction
