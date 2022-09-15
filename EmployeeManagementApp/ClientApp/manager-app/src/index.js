0e54bb4653b1a24bc83d16077ee8c1e4f424d686
vmarcell96 <vajdam96@gmail.com>
2022-09-15 16:31:24 +0200

    refresh tokens only work without strict mode

--- EmployeeManagementApp/ClientApp/manager-app/src/index.js
+++ EmployeeManagementApp/ClientApp/manager-app/src/index.js
@@ -17,7 +17,6 @@ import UpdateUser from './components/Users/UpdateUser';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <BrowserRouter>
       <FlashMessageProvider>
       <AuthProvider>
@@ -36,5 +35,4 @@ root.render(
       </AuthProvider>
       </FlashMessageProvider>
     </BrowserRouter>
-  </React.StrictMode>
 );
