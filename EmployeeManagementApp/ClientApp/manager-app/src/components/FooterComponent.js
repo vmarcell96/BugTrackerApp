72e0b204e35192ffee09a83693ebd96e1fb93676
vmarcell96 <vajdam96@gmail.com>
2022-10-12 22:58:40 +0200

    created footer component

---
+++ EmployeeManagementApp/ClientApp/manager-app/src/components/FooterComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+      <footer>
+        This is the footer
+      </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
