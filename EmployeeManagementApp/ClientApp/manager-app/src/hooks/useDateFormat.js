7174408794940fc24dc171e1896b456c9a3b420e
vmarcell96 <vajdam96@gmail.com>
2022-10-12 10:19:35 +0200

    hook for formatting date

---
+++ EmployeeManagementApp/ClientApp/manager-app/src/hooks/useDateFormat.js
@@ -0,0 +1,10 @@
+import moment from "moment/moment";
+
+const useDateFormat = () => {
+    const dateFormat = (value) => {
+        return moment(value).format('YYYY/MM/DD');
+    }
+    return dateFormat;
+}
+
+export default useDateFormat;
