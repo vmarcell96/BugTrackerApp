f824e552d172297d57a7b09f4120f189d7a902bf
vmarcell96 <vajdam96@gmail.com>
2022-10-18 00:36:03 +0200

    footer component implemented

--- EmployeeManagementApp/ClientApp/manager-app/src/components/FooterComponent.js
+++ EmployeeManagementApp/ClientApp/manager-app/src/components/FooterComponent.js
@@ -2,14 +2,31 @@ import React from 'react'
 import { Navbar, Nav, Container, Card } from 'react-bootstrap'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBug } from "@fortawesome/free-solid-svg-icons";
+import { faGithubSquare, faTwitterSquare, faInstagramSquare, faFacebookSquare } from '@fortawesome/free-brands-svg-icons'
 
 const FooterComponent = () => {
   return (
-    <footer className="footer">
-
-      <p id="footer-content"><span id="footer-logo"><FontAwesomeIcon icon={faBug} />Tracker</span> © Copyright 2022 BugTracker</p>
-
-    </footer>
+    <Container className="footer-container">
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div className="col-md-4 d-flex align-items-center">
+          <span id="footer-logo"><FontAwesomeIcon icon={faBug} />Tracker</span> © Copyright 2022 BugTracker
+        </div>
+        <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+          <li className="ms-3">
+            <FontAwesomeIcon icon={faGithubSquare} className="footer-icon" />
+          </li>
+          <li className="ms-3">
+            <FontAwesomeIcon icon={faTwitterSquare} className="footer-icon" />
+          </li>
+          <li className="ms-3">
+            <FontAwesomeIcon icon={faInstagramSquare} className="footer-icon" />
+          </li>
+          <li className="ms-3">
+            <FontAwesomeIcon icon={faFacebookSquare} className="footer-icon" />
+          </li>
+        </ul>
+      </footer>
+    </Container>
   )
 }
 
