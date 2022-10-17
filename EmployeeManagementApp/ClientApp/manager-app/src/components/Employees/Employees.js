c744fe9ad12a6676f4c509f9deb7b0facacba241
vmarcell96 <vajdam96@gmail.com>
2022-10-18 00:35:01 +0200

    refactored user and employee components

--- EmployeeManagementApp/ClientApp/manager-app/src/components/Employees/Employees.js
+++ EmployeeManagementApp/ClientApp/manager-app/src/components/Employees/Employees.js
@@ -5,113 +5,106 @@ import React from 'react'
 import { Button, Table, Card, Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faPlus, faEdit } from "@fortawesome/free-solid-svg-icons";
-import LoadingSpin from "react-loading-spin";
 import { useNavigate } from "react-router-dom";
 import useAuth from '../../hooks/useAuth';
 import useDateFormat from '../../hooks/useDateFormat';
 
 const Employees = () => {
-	const { auth } = useAuth();
-	const dateFormat = useDateFormat();
-	let navigate = useNavigate();
-	const [employees, error, loading, axiosFetch] = useAxiosFunction();
+  const { auth } = useAuth();
+  const dateFormat = useDateFormat();
+  let navigate = useNavigate();
+  const [data, setData, error, loading, axiosFetch] = useAxiosFunction();
+
+  const getData = async () => {
+    await axiosFetch({
+      axiosInstance: axios,
+      method: 'GET',
+      url: '/api/employees',
+    });
+  };
+
+  useEffect(() => {
+    getData();
+    // eslint-disable-next-line
+  }, [])
+
+  const handleDelete = async (id) => {
+    const filteredData = data.filter(e => e.id !== id);
+    await axiosFetch({
+      axiosInstance: axios,
+      method: 'DELETE',
+      url: `/api/employees/${id}`,
+    })
+    setData(filteredData);
+  }
+
+  const navToUpdate = (id, employee) => {
+    navigate(`/employees/${id}`, { state: employee })
+  }
+
+  return (
+    <>
+      <Container>
+        <Card>
+        <Card.Title className='page-title'><h2>Employees</h2></Card.Title>
+        <Card.Body>
+          <div className="table-responsive" style={{ maxWidth: '100%' }}>
+            {loading &&
+              <div className="d-flex justify-content-center">
+                <div className="spinner-border" role="status">
+                  <span className="sr-only">Loading...</span>
+                </div>
+              </div>}
+            {!loading && error && <p style={{ color: "red" }}>{error}</p>}
+            {!loading && !error && data &&
+              <>
+                <Table borderless>
+                  <thead>
+                    <tr>
+                      <th>First Name</th>
+                      <th>Last Name</th>
+                      <th>Hiring Date</th>
+                      <th>
+                        {auth?.role === "Admin" && <>
+                          <Button className='button plus' onClick={() => { navigate("/employees/add") }}>
+                            <FontAwesomeIcon icon={faPlus} />
+                          </Button>
+                        </>}
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="table-group-divider">
+                    {data && data.map((employee) =>
+                      <tr key={employee.id}>
+                        <td>{employee.firstName}</td>
+                        <td>{employee.lastName}</td>
+                        <td>{dateFormat(employee.hiringDate)}</td>
 
 
-	//cosmetic
-	const [isPendingDelete, setIsPendingDelete] = useState(false);
-
-	const getData = () => {
-		axiosFetch({
-			axiosInstance: axios,
-			method: 'GET',
-			url: '/api/employees',
-		});
-	}
-
-	useEffect(() => {
-		getData();
-		// eslint-disable-next-line
-	}, [])
-
-	const handleDelete = (id) => {
-		setIsPendingDelete(true);
-		axiosFetch({
-			axiosInstance: axios,
-			method: 'DELETE',
-			url: `/api/employees/${id}`,
-		})
-		setTimeout(() => { getData() }, 1000);
-		setTimeout(() => { setIsPendingDelete(false) }, 1000);
-	}
-
-	const navToUpdate = (id, employee) => {
-		navigate(`/employees/${id}`, { state: employee })
-	}
-
-	return (
-			<Container>
-		<Card>
-			<div className="table-responsive" style={{ maxWidth: '100%' }}>
-				{loading &&
-					<div className="d-flex justify-content-center">
-						<div className="spinner-border" role="status">
-							<span className="sr-only">Loading...</span>
-						</div>
-					</div>}
-				{error && <p style={{ color: "red" }}>{error}</p>}
-				{!loading && !error && employees &&
-					<>
-						<Table borderless>
-							<thead>
-								<tr>
-									<th>First Name</th>
-									<th>Last Name</th>
-									<th>Hiring Date</th>
-									<th>
-										{auth?.role === "Admin" && <>
-											<Button className='button plus' onClick={() => { navigate("/employees/add") }}>
-												<FontAwesomeIcon icon={faPlus} />
-											</Button>
-										</>}
-									</th>
-								</tr>
-							</thead>
-							<tbody className="table-group-divider">
-								{employees !== true && employees.map((employee) =>
-									<tr key={employee.id}>
-										<td>{employee.firstName}</td>
-										<td>{employee.lastName}</td>
-										<td>{dateFormat(employee.hiringDate)}</td>
-
-
-										<td>
-											{auth?.role === "Admin" &&
-												(!isPendingDelete ?
-													<>
-														<Button className='button' onClick={() => { navToUpdate(employee.id, employee) }}>
-															<FontAwesomeIcon icon={faEdit} />
-														</Button>
-														<Button className='button' onClick={() => { handleDelete(employee.id) }}>
-															<FontAwesomeIcon icon={faTrash} />
-														</Button>
-													</>
-													:
-													<><div className="d-flex justify-content-center">
-														<div className="spinner-border" role="status">
-															<span className="sr-only">Loading...</span>
-														</div>
-													</div></>)}
-										</td>
-									</tr>
-								)}
-							</tbody>
-						</Table>
-					</>
-				}
-			</div>
-		</Card>
-		</Container>
-	)
+                        <td>
+                          {auth?.role === "Admin" &&
+                            <>
+                              <Button className='button' onClick={() => { navToUpdate(employee.id, employee) }}>
+                                <FontAwesomeIcon icon={faEdit} />
+                              </Button>
+                              <Button className='button' onClick={() => { handleDelete(employee.id) }}>
+                                <FontAwesomeIcon icon={faTrash} />
+                              </Button>
+                            </>
+                          }
+                        </td>
+                      </tr>
+                    )}
+                  </tbody>
+                </Table>
+              </>
+            }
+          </div>
+        </Card.Body>
+        </Card>
+      </Container>
+    </>
+  )
 }
 
 export default Employees
--- EmployeeManagementApp/ClientApp/manager-app/src/components/Users/AddUser.js
+++ EmployeeManagementApp/ClientApp/manager-app/src/components/Users/AddUser.js
@@ -1,26 +1,23 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import LoadingSpin from "react-loading-spin";
-import { Button, Card, Dropdown } from "react-bootstrap";
+import { Button, Card, Dropdown, Container } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import axios from "../../apis/axiosInstance";
 import useAxiosFunction from "../../hooks/useAxiosFunction";
 
 function AddUser() {
-  const [isPendingAdd, setIsPendingAdd] = useState(false);
   const [postFirstName, setPostFirstName] = useState("");
   const [postLastName, setPostLastName] = useState("");
   const [postUsername, setPostUsername] = useState("");
   const [postPassword, setPostPassword] = useState("");
   const [postRole, setPostRole] = useState("User");
   let navigate = useNavigate();
-  const [users, error, loading, axiosFetch] = useAxiosFunction();
+  const [data, setData, error, loading, axiosFetch] = useAxiosFunction();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    setIsPendingAdd(true);
-    axiosFetch({
+    await axiosFetch({
       axiosInstance: axios,
       method: "POST",
       url: "/api/users",
@@ -32,86 +29,81 @@ function AddUser() {
         Role: `${postRole}`,
       },
     });
-    setTimeout(() => {
-      setIsPendingAdd(false);
-    }, 1000);
-    setTimeout(() => {
-      navigate(-1);
-    }, 1000);
+    navigate(-1);
   };
 
   return (
-    <Card body>
-      <Form
-        onSubmit={(e) => {
-          handleSubmit(e);
-        }}
-        className="form"
-      >
-        <Form.Group className="mb-3" controlId="formBasicText">
-          <Form.Label>First Name:</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter first name"
-            required
-            value={postFirstName}
-            onChange={(e) => setPostFirstName(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicText">
-          <Form.Label>Last Name:</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter last name"
-            required
-            value={postLastName}
-            onChange={(e) => setPostLastName(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicText">
-          <Form.Label>Username:</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter username"
-            required
-            value={postUsername}
-            onChange={(e) => setPostUsername(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicText">
-          <Form.Label>Password:</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter password"
-            required
-            value={postPassword}
-            onChange={(e) => setPostPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicText">
-          <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
-              {postRole}
-            </Dropdown.Toggle>
+    <Container>
+      <Card className="w-100">
+        <Card.Title>Add new employee</Card.Title>
+        {!loading && !error && <Card.Body>
+          <Form onSubmit={(e) => { handleSubmit(e) }} className="form">
+            <Form.Group className="mb-3" controlId="formBasicText">
+              <Form.Label>First Name:</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter first name"
+                required
+                value={postFirstName}
+                onChange={(e) => setPostFirstName(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicText">
+              <Form.Label>Last Name:</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter last name"
+                required
+                value={postLastName}
+                onChange={(e) => setPostLastName(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicText">
+              <Form.Label>Username:</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter username"
+                required
+                value={postUsername}
+                onChange={(e) => setPostUsername(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicText">
+              <Form.Label>Password:</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter password"
+                required
+                value={postPassword}
+                onChange={(e) => setPostPassword(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicText">
+              <Dropdown>
+                <Dropdown.Toggle className="button" id="dropdown-basic">
+                  {postRole}
+                </Dropdown.Toggle>
 
-            <Dropdown.Menu>
-              <Dropdown.Item value="User" onClick={(e) => setPostRole("User")}>User</Dropdown.Item>
-              <Dropdown.Item value="Admin" onClick={(e) => setPostRole("Admin")}>Admin</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </Form.Group>
-        {!isPendingAdd && (
-          <Button variant="primary" type="submit">
-            Add user
-          </Button>
-        )}
-        {isPendingAdd && (
-          <button>
-            <LoadingSpin />
-          </button>
-        )}
-      </Form>
-    </Card>
+                <Dropdown.Menu>
+                  <Dropdown.Item value="User" onClick={(e) => setPostRole("User")}>User</Dropdown.Item>
+                  <Dropdown.Item value="Admin" onClick={(e) => setPostRole("Admin")}>Admin</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Form.Group>
+            <Button className="button" type="submit">
+              Add user
+            </Button>
+
+            {loading && !error &&
+              <div className="d-flex justify-content-center">
+                <div className="spinner-border" role="status">
+                  <span className="sr-only">Loading...</span>
+                </div>
+              </div>}
+          </Form>
+        </Card.Body>}
+      </Card>
+    </Container>
   );
 }
 
